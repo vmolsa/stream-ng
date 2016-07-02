709b235c0ff8fd9610ac80e4e0d15b8610b08134
vmolsa <ville.molsa@gmail.com>
2016-07-02 13:03:17 +0300

    Update example

--- examples/stream-compare.js
+++ examples/stream-compare.js
@@ -1,44 +1,53 @@
 var fs = require('fs');
-var StreamNG = require('../index.js');
+var StreamNg = require('../index.js');
 var stream = require('stream');
-var _ = StreamNG._;
 
 var out = fs.openSync('./out', 'w');
 var out2 = fs.openSync('./out2', 'w');
 
 function testStreamNG(callback) {
-  var time = _.now();
+  var time = Date.now();
 
-  var writable = new StreamNG({ objectMode: true, write: function(chunk, next) {
+  var writable = new StreamNg({ objectMode: true, write: function(chunk, next) {
     fs.write(out, chunk, 0, chunk.length, next);
   }});
 
   writable.then(function() {
-    var end = _.now();
+    var end = Date.now();
     console.log('StreamNG Done: ' + (end - time) + 'ms');
-    _.delay(callback);
+    setImmediate(callback);
   }).catch(function(error) {
     throw error;
+  }).open(function() {
+    console.log('Open');
+  }).close(function() {
+    console.log('Close');
+  }).drain(function() {
+    console.log('Drain');
+  }).pause(function() {
+    console.log('Pause');
+  }).resume(function() {
+    console.log('Resume');
   });
   
   for (var index = 0; index < 1024; index++) {
     writable.write(new Buffer(1024 * 10 + index));
   }
   
-  writable.end();
+  writable.end('Done!');
 }
 
 function testStream(callback) {
-  var time = _.now();
+  var time = Date.now();
   
   var writable = new stream.Writable({ objectMode: true, write: function(chunk, encoding, next) {
     fs.write(out2, chunk, 0, chunk.length, next);
   }});
 
   writable.on('finish', function() {
-    var end = _.now();
+    var end = Date.now();
     console.log('Stream Done: ' + (end - time) + 'ms');
-    _.delay(callback);
+    setImmediate(callback);
   });
   
   for (var index = 0; index < 1024; index++) {
